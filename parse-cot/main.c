f0d2b7dee28b509100d2bd0bd9bfbac63c3fd1cc
gygias <david@dpd-rmbp.attlocal.net>
2017-04-08 15:34:55 -0700

    add download capability and generalize structures

--- parse-cot/main.c
+++ parse-cot/main.c
@@ -10,15 +10,19 @@
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <limits.h>
 #include <math.h>
+#include <curl/curl.h>
 
-#define URL "http://www.cftc.gov/dea/futures/nat_gas_sf.htm"
-#define NG_0 "Disaggregated Commitments"
-#define NG_D "----------"
+#define NG_URL "http://www.cftc.gov/dea/futures/nat_gas_sf.htm"
+#define PET_URL "http://www.cftc.gov/dea/futures/petroleum_sf.htm"
+#define OTHER_URL "http://www.cftc.gov/dea/futures/other_sf.htm"
+#define NODE_BEGIN "Disaggregated Commitments of Traders"
+#define NODE_END "----------"
 #define DOT_INT INT_MIN
 #define DOT_FLOAT NAN
 
@@ -151,7 +155,7 @@ int get_floats(const char *line, float **numbers, int dots)
     return nNums;
 }
 
-typedef struct ng_p_c_or_n {
+typedef struct p_c_or_n {
     int pLong;
     int pShort;
     int sLong;
@@ -163,9 +167,9 @@ typedef struct ng_p_c_or_n {
     int oLong;
     int oShort;
     int oSpreading;
-} ng_p_c_or_n;
+} p_c_or_n;
 
-typedef struct ng_poic {
+typedef struct poic {
     float pLong;
     float pShort;
     float sLong;
@@ -177,41 +181,55 @@ typedef struct ng_poic {
     float oLong;
     float oShort;
     float oSpreading;
-} ng_poic;
+} poic;
 
-typedef struct ng_node {
-    ng_p_c_or_n p;
-    ng_p_c_or_n c;
-    ng_poic poic;
-    ng_p_c_or_n n;
-} ng_node;
+typedef struct node {
+    p_c_or_n p;
+    p_c_or_n c;
+    poic poic;
+    p_c_or_n n;
+} node;
 
-int parse_ng(FILE *fd, ng_node **outNodes)
+int parse(FILE *fd, node **outNodes)
 {
-    char *line;
+    char *line = NULL;
+    
+    while(1) {
+        char *aLine = my_readline(fd, 1);
+        char *txtBegin = strstr(aLine, NODE_BEGIN);
+        if ( txtBegin ) {
+            line = txtBegin;
+            break;
+        }
+    }
+    
+    if ( ! line ) FAIL("couldn't find txt");
+    
     int nodesSize = 52, nNodes = 0; // as of 4/2017
     int *nums,nNums;
     float *floats;
-    ng_node *nodes = calloc(nodesSize,sizeof(ng_node));
+    node *nodes = calloc(nodesSize,sizeof(node));
     for (int i = 0; ; i++ ) {
-        line = my_readline(fd,1);
-        int prefLen = strlen(NG_0);
-        short inset = ( i == 0 ) ? 1 : 0; // xxx
-        if ( strncmp( NG_0, line + inset, prefLen ) != 0 )
+        //line = my_readline(fd,1);
+        int prefLen = strlen(NODE_BEGIN);
+        //short inset = ( i == 0 ) ? 1 : 0; // xxx
+        if ( strncmp( NODE_BEGIN, line, prefLen ) != 0 )
             break;
         line = my_readline(fd, 7);
         printf("parsing '%s'",line);
         
         line = my_readline(fd, 1);
         char *code;
+        int openInterest;
         line = get_code(line, &code);
         nNums = get_ints(line, &nums, 0);
-        if ( nNums != 1 ) FAIL("NG Code");
-        printf("\tcftc id: %s, open interest: %d\n",code,nums[0]);
+        if ( nNums != 1 ) FAIL("Code");
+        openInterest = nums[0];
+        printf("\tcftc id: %s, open interest: %d\n",code,openInterest);
         
         line = my_readline(fd, 2);
         nNums = get_ints(line, &nums, 0);
-        if ( nNums != 11 ) FAIL("NG Positions");
+        if ( nNums != 11 ) FAIL("Positions");
         printf("positions: ");
         for(int j = 0; j < nNums; j++) {
             if(nums[j]==DOT_INT)
@@ -224,7 +242,7 @@ int parse_ng(FILE *fd, ng_node **outNodes)
         
         line = my_readline(fd, 3);
         nNums = get_ints(line, &nums, 1);
-        if ( nNums != 11 ) FAIL("NG Changes");
+        if ( nNums != 11 ) FAIL("Changes");
         printf("changes: ");
         for(int j = 0; j < nNums; j++) {
             if(nums[j]==DOT_INT)
@@ -250,7 +268,7 @@ int parse_ng(FILE *fd, ng_node **outNodes)
         
         line = my_readline(fd, 3);
         nNums = get_ints(line, &nums, 1);
-        if ( nNums != 11 ) FAIL("NG Traders");
+        if ( nNums != 11 ) FAIL("Traders");
         printf("traders: ");
         for(int j = 0; j < nNums; j++) {
             if(nums[j]==DOT_INT)
@@ -262,10 +280,12 @@ int parse_ng(FILE *fd, ng_node **outNodes)
         free(nums);
         
         line = my_readline(fd, 1);
-        prefLen = strlen(NG_D);
-        if ( strncmp(line,NG_D,prefLen) != 0 ) FAIL("NG footer");
+        prefLen = strlen(NODE_END);
+        if ( strncmp(line,NODE_END,prefLen) != 0 ) FAIL("footer");
         
         nNodes++;
+        
+        line = my_readline(fd, 1);
     }
     
     *outNodes = nodes;
@@ -273,30 +293,58 @@ int parse_ng(FILE *fd, ng_node **outNodes)
     return nNodes;
 }
 
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
     
-    if ( argc < 3 ) {
-        fprintf(stderr,"usage: parse-cot <file> <n|c|g>\n");
+    if ( argc < 2 ) {
+        fprintf(stderr,"usage: parse-cot <n|c|g>\n");
         return 1;
     }
     
-    const char *file = argv[1];
-    const char *com = argv[2];
-    
-    FILE *fd = fopen(file, "r");
-    if ( fd == NULL ) {
-        fprintf(stderr,"fopen(%s) failed: %s\n",file,strerror(errno));
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char *url;
+    if ( argv[1][0] == 'n' )
+        url = NG_URL;
+    else if ( argv[1][0] == 'c' )
+        url = PET_URL;
+    else if ( argv[1][0] == 'g' )
+        url = OTHER_URL;
+    else
+        FAIL("unknown commodity type");
+    char *template = strdup("/tmp/parse-cot-XXXXXXXXXXX");
+    int tempFd = mkstemp(template);
+    if ( tempFd == -1 ) FAIL("mkstemp");
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fdopen(tempFd,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        fclose(fp);
+        close(tempFd);
+    } else FAIL("curl");
+
+    fp = fopen(template, "r");
+    if ( fp == NULL ) {
+        fprintf(stderr,"fopen(%s) failed: %s\n",template,strerror(errno));
         return 1;
     }
     
-    if ( strcmp(com,"n") == 0 ) {
-        ng_node *nodes;
-        int nNodes = parse_ng(fd,&nodes);
-        printf("parsed %d ng nodes\n",nNodes);
-    }
+    node *nodes;
+    int nNodes = parse(fp,&nodes);
+    printf("parsed %d nodes\n",nNodes);
     
-    fclose(fd);
+    fclose(fp);
+    unlink(template);
     
     return 0;
 }
